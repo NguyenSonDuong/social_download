9d458cc28c35bcc27690fb67236da0815742bbd6
nguyensonduong <nguyenduong08041999@gmail.com>
2025-02-06 18:57:09 +0700

    thay đổi thành tải nhiều video với nhiều link

--- douyin/douyin_key.py
+++ douyin/douyin_key.py
@@ -1,4 +1,4 @@
 class Option:
     def __init__(self):
-        self.video_url = ""
+        self.video_url = []
         self.output_path = "Downloads"
--- douyin/douyin_server_one.py
+++ douyin/douyin_server_one.py
@@ -5,69 +5,55 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from douyin.douyin_key import Option
+import re
 import requests
 
 class Douyin:
     _process = None
+
     def __init__(self, process, Option = Option()):
         self._process = process
         self.option = Option
-        pass
-
-    def get_video_source(self):
-        # Lấy mã nguồn của trang
-        page_source = self.get_page_source(self.option.video_url)
-        
-        # Sử dụng BeautifulSoup để phân tích cú pháp HTML
-        soup = BeautifulSoup(page_source, 'html.parser')
-        
-        # Tìm thẻ <video> và thẻ <source> bên trong nó
-        video_tag = soup.find('video')
-        if video_tag:
-            source_tag = video_tag.find('source')
-            if source_tag and 'src' in source_tag.attrs:
-                return source_tag['src']
-        
-        return None
-    def get_page_source(self):
-        # Đường dẫn đến ChromeDriver
-        
-        # Tùy chọn cho Chrome
         chrome_options = Options()
-        chrome_options.add_argument("--headless")  # Chạy Chrome ở chế độ headless (không hiển thị giao diện)
-        
-        # Khởi tạo trình điều khiển Chrome
-        driver = webdriver.Chrome( options=chrome_options)
-        
-        # Mở URL
-        driver.get(self.option.video_url)
-        
-        # Chờ đối tượng có id 'RENDER_DATA' xuất hiện
-        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "RENDER_DATA")))
-        
-        # Lấy mã nguồn của trang
-        page_source = driver.page_source
-        
-        # Đóng trình duyệt
-        driver.quit()
-        
-        return page_source
+        chrome_options.add_argument("--headless")
+        self.driver = webdriver.Chrome( options=chrome_options)
 
-    def download_video(self,video_url):
-        """
-        Downloads the video from the given URL and saves it to the specified file.
-        
-        :param video_url: The URL of the video to download.
-        :param file_name: The name of the file to save the video as.
-        """
-        response = requests.get(f"https:{video_url}", stream=True)
-        if response.status_code == 200:
-            with open(self.option.output_path+"/douyin.mp4", 'wb') as f:
-                for chunk in response.iter_content(chunk_size=1024):
-                    f.write(chunk)
-            print(f"Video downloaded successfully: {self.option.output_path+"/douyin.mp4"}")
-        else:
-            print(f"Failed to download video. Status code: {response.status_code}")
+    def clean_chinese_text(text):
+        cleaned_text = re.sub(r"[^\u4e00-\u9fff]", "", text)
+        return cleaned_text
+    
+    def get_page_sources(self):
+
+        urls = []
+        for url in self.option.video_url:
+            self.driver =  self.driver.get(url)
+            WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.ID, "RENDER_DATA")))
+            page_source = self.driver.page_source
+            soup = BeautifulSoup(page_source, 'html.parser')
+            video_tag = soup.find('video')
+            title = soup.find('title')
+            if video_tag:
+                source_tag = video_tag.find('source')
+                if source_tag and 'src' in source_tag.attrs:
+                    # urls.append()
+                    urls.append({
+                        "url": source_tag['src'],
+                        "title": title
+                    })
+        self.driver.quit()
+        return urls
+
+    def download_video(self,video_urls):
+        for url in video_urls:
+            filename = f"{self.option.output_path}/{self.clean_chinese_text(url["title"])}.mp4"
+            response = requests.get(f"https:{url["url"]}")
+            if response.status_code == 200:
+                with open(filename, 'wb') as f:
+                    for chunk in response.iter_content(chunk_size=1024):
+                        f.write(chunk)
+                print(f"Video downloaded successfully: {filename}")
+            else:
+                print(f"Failed to download video. Status code: {response.status_code}")
 
 
    
